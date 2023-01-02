bb1339264bea9d8659de69ef853e37a5eaf9af02
zainab66 <zainab66r@gmail.com>
2023-01-02 14:48:18 -0500

    t

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Projects from './screen/Projects';
+import Header from './screen/Header';
 
 const users = [
   {
@@ -15,63 +18,66 @@ const users = [
 ];
 
 function App() {
-  const [subdomain, setSubDomain] = useState(null);
-  useEffect(() => {
-    const host = window.location.host; // gets the full domain of the app
-    console.log(
-      'host',
-      host
-        .split('.')
-        .slice(0, host.includes('subdomain-test.onrender.com') ? -1 : -2)
+  if (window.location.host.split('.')[0] === 'projects') {
+    // It checks if the subdomain is named projects.
+    return (
+      <Router>
+        <Header />
+        <Projects />
+      </Router>
     );
-    const arr = host
-      .split('.')
-      .slice(0, host.includes('subdomain-test.onrender.com') ? -1 : -2);
-    if (arr.length > 0) setSubDomain(arr[0]);
-  }, []);
-  const requestedUser = users.find((user) => user.username === subdomain);
-  return (
-    <div className="app">
-      ss {subdomain} ww{window.location.host}
-      {subdomain ? (
-        requestedUser ? (
-          <div>
-            <h1>Username</h1>
-            <h3>{requestedUser.username}</h3>
-            <h1>Age</h1>
-            <h3>{requestedUser.age}</h3>
-            <h1>Hobbies</h1>
-            <ul>
-              {requestedUser.hobbies.map((hobby) => (
-                <li key={hobby}>{hobby}</li>
-              ))}
-            </ul>
-          </div>
-        ) : (
-          <h1>Not Found</h1>
-        )
-      ) : (
-        <div>
-          {users.map((user) => (
-            <div>
-              <a
-                key={user.username}
-                href={
-                  window.location.protocol +
-                  '//' +
-                  user.username +
-                  '.' +
-                  window.location.host
-                }
-              >
-                {user.username}
-              </a>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  }
 }
-
 export default App;
+
+// function App() {
+//   const [subdomain, setSubDomain] = useState(null);
+//   useEffect(() => {
+//     const host = window.location.host; // gets the full domain of the app
+
+//     const arr = host.split('.').slice(0, host.includes('localhost') ? -1 : -2);
+//     if (arr.length > 0) setSubDomain(arr[0]);
+//   }, []);
+//   const requestedUser = users.find((user) => user.username === subdomain);
+//   return (
+//     <div className="app">
+//       {subdomain ? (
+//         requestedUser ? (
+//           <div>
+//             <h1>Username</h1>
+//             <h3>{requestedUser.username}</h3>
+//             <h1>Age</h1>
+//             <h3>{requestedUser.age}</h3>
+//             <h1>Hobbies</h1>
+//             <ul>
+//               {requestedUser.hobbies.map((hobby) => (
+//                 <li key={hobby}>{hobby}</li>
+//               ))}
+//             </ul>
+//           </div>
+//         ) : (
+//           <h1>Not Found</h1>
+//         )
+//       ) : (
+//         <div>
+//           {users.map((user) => (
+//             <div>
+//               <a
+//                 key={user.username}
+//                 href={
+//                   window.location.protocol +
+//                   '//' +
+//                   user.username +
+//                   '.' +
+//                   window.location.host
+//                 }
+//               >
+//                 {user.username}
+//               </a>
+//             </div>
+//           ))}
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
